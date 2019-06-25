8ca6663bd9c4e8d3fc5da49f8b9076c26ca96dd1
Pham Minh Tri <phamtri2395@gmail.com>
2019-06-26 00:42:37 +0700

    chore(q1): ignore local storage file

--- packages/q1/src/constants/connections.ts
+++ packages/q1/src/constants/connections.ts
@@ -1,5 +1,5 @@
 import { resolve } from 'path';
 
-export const LOCAL_STORAGE = resolve(__dirname, '../storage/local.sql');
+export const LOCAL_STORAGE = resolve(__dirname, '../../storage/local.tmp');
 
 export const ENTITY_GLOB = resolve(__dirname, '../**/*.entity{.ts,.js}');
